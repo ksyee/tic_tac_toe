25dce38e932f6788d5428a718fb6aa1223a0c3f8
KSY <tjssud0647@gmail.com>
2023-06-09 15:43:14 +0900

    complete player-board function.

--- js/common.js
+++ js/common.js
@@ -6,9 +6,20 @@ const errorMsg = document.querySelector('#error-msg');
 
 const editBtn = document.querySelectorAll('.btn-edit');
 const cancelBtn = document.querySelector('#cancel');
-const submitBtn = document.querySelector('form button[type="submit"]');
+const inputPlayerName = document.querySelector('input[name="playername"]');
+const players = [
+  {
+    name: 'player1',
+    symbol: 'X',
+  },
+  {
+    name: 'player2',
+    symbol: 'O',
+  },
+];
 
 let playerIndex = 0;
+
 
 editBtn.forEach((item, index) => {
   item.addEventListener('click', () => {
@@ -21,25 +32,43 @@ editBtn.forEach((item, index) => {
 cancelBtn.addEventListener('click', () => {
   modal.classList.remove('on');
   backdrop.classList.remove('on');
+  form.firstElementChild.classList.remove('error');
   errorMsg.textContent = '';
 });
 
 backdrop.addEventListener('click', () => {
   modal.classList.remove('on');
   backdrop.classList.remove('on');
+  form.firstElementChild.classList.remove('error');
+  inputPlayerName.value = '';
   errorMsg.textContent = '';
 });
 
-submitBtn.addEventListener('click', e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
-  const formData = new FormData(form);
+  const formData = new FormData(e.target);
   const name = formData.get('playername').trim();
   if (!name) {
+    form.firstElementChild.classList.add('error');
     errorMsg.textContent = '플레이어 이름을 입력해주세요.';
     return;
   } else {
-    playerIndex === 0 ? (playerName[playerIndex].textContent = name) : (playerName[playerIndex].textContent = name);
+    if (playerIndex === 0) {
+      players[0].name = name;
+      playerName[0].textContent = name;
+    } else {
+      players[1].name = name;
+      playerName[1].textContent = name;
+    }
     modal.classList.remove('on');
     backdrop.classList.remove('on');
+    inputPlayerName.value = '';
   }
+  console.log(players);
+});
+
+inputPlayerName.addEventListener('focus', () => {
+  form.firstElementChild.classList.remove('error');
+  errorMsg.textContent = '';
+  return;
 });
